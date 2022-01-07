64b01df055a140c91a9cbb6a9e634ff1b3011f7d
Ron Jones <ron@ovao.dev>
2022-01-06 16:14:25 -0800

    Add tracking code; remove extraneous Google Fonts code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { Theme } from '$lib/stores/theme'
-  //import { dev } from '$app/env' // TODO: bring in tracking script
+  import { dev } from '$app/env'
   import { started } from '$lib/stores/core'
   import { onMount } from 'svelte'
   import { theme, createSubscriber } from '$lib/stores/theme'
@@ -33,16 +33,59 @@
 </script>
 
 <svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link
-    rel="preconnect"
-    href="https://fonts.gstatic.com"
-    crossorigin="anonymous"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap"
-    rel="stylesheet"
-  />
+  {#if !dev}
+    <script async>
+      ;(function (window, document, dataLayerName, id) {
+        ;(window[dataLayerName] = window[dataLayerName] || []),
+          window[dataLayerName].push({
+            start: new Date().getTime(),
+            event: 'stg.start'
+          })
+        var scripts = document.getElementsByTagName('script')[0],
+          tags = document.createElement('script')
+        function stgCreateCookie(a, b, c) {
+          var d = ''
+          if (c) {
+            var e = new Date()
+            e.setTime(e.getTime() + 24 * c * 60 * 60 * 1e3),
+              (d = '; expires=' + e.toUTCString())
+          }
+          document.cookie = a + '=' + b + d + '; path=/'
+        }
+        var isStgDebug =
+          (window.location.href.match('stg_debug') ||
+            document.cookie.match('stg_debug')) &&
+          !window.location.href.match('stg_disable_debug')
+        stgCreateCookie('stg_debug', isStgDebug ? 1 : '', isStgDebug ? 14 : -1)
+        var qP = []
+        dataLayerName !== 'dataLayer' &&
+          qP.push('data_layer_name=' + dataLayerName),
+          isStgDebug && qP.push('stg_debug')
+        var qPString = qP.length > 0 ? '?' + qP.join('&') : ''
+        ;(tags.async = !0),
+          (tags.src =
+            'https://ovao.containers.piwik.pro/' + id + '.js' + qPString),
+          scripts.parentNode.insertBefore(tags, scripts)
+        !(function (a, n, i) {
+          a[n] = a[n] || {}
+          for (var c = 0; c < i.length; c++)
+            !(function (i) {
+              ;(a[n][i] = a[n][i] || {}),
+                (a[n][i].api =
+                  a[n][i].api ||
+                  function () {
+                    var a = [].slice.call(arguments, 0)
+                    'string' == typeof a[0] &&
+                      window[dataLayerName].push({
+                        event: n + '.' + i + ':' + a[0],
+                        parameters: [].slice.call(arguments, 1)
+                      })
+                  })
+            })(i[c])
+        })(window, 'ppms', ['tm', 'cm'])
+      })(window, document, 'dataLayer', 'b2acc90b-7fb0-4a4e-bb74-1cc80c82a685')
+    </script>
+  {/if}
 </svelte:head>
 
 <main>
