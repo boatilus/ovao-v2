6283434a96ca307dd5f451bd512e9b7330924c6b
Ron Jones <ron@ovao.dev>
2022-01-06 17:22:59 -0800

    Remove usage of :global from /posts

---
+++ src/routes/posts/__layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { theme } from '$lib/stores/theme'
+
+  import './global.scss'
+  import themes from '../../themes.json'
+
+  theme.set(themes['/posts*'])
+</script>
+
+<div id="posts--container">
+  <slot />
+</div>
+
+<style lang="scss">
+  @import './src/_core';
+
+  #posts--container {
+    @include desktop {
+      margin: 0 4vw;
+      padding: 3.5em 0;
+    }
+  }
+</style>
