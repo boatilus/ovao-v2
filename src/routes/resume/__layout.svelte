dd7f6ff372569d362237d9a533f540adcd86d31a
Ron Jones <ron@ovao.dev>
2022-01-11 13:56:26 -0800

    Remove font load; handled in main layout

--- src/routes/resume/__layout.svelte
+++ src/routes/resume/__layout.svelte
@@ -6,33 +6,14 @@
 </script>
 
 <script lang="ts">
-  import { onMount } from 'svelte'
   import { theme } from '$lib/stores/theme'
   import themes from '../../themes.json'
 
   theme.set(themes['/resume*'])
-
-  onMount(() => {
-    let stylesheet = document.createElement('link')
-    stylesheet.href =
-      'https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500&display=swap'
-    stylesheet.rel = 'stylesheet'
-    // Temporarily set media to something inapplicable to ensure it'll fetch
-    // without blocking rendering.
-    stylesheet.media = 'only x'
-
-    // Set the media back when the stylesheet's loaded.
-    stylesheet.onload = () => {
-      stylesheet.media = 'all'
-    }
-
-    document.getElementsByTagName('head')[0].appendChild(stylesheet)
-  })
 </script>
 
 <svelte:head>
   <title>Résumé - Ron Jones</title>
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
   {@html serialize(person)}
 </svelte:head>
 
