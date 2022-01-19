c695d628f337fd425020d50a0b21558e4c7384b4
Ron Jones <ron@ovao.dev>
2022-01-19 11:36:12 -0800

    Update sitemap with blog posts

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit'
 import { DOMAIN } from '$lib/variables'
+import { getPosts } from '$lib/get_posts'
 
 export const get: RequestHandler = async () => {
   const headers = {
@@ -8,6 +9,11 @@ export const get: RequestHandler = async () => {
   }
 
   const paths = ['', '/resume', '/works']
+
+  const posts = await getPosts()
+  for (const { slug } of posts) {
+    paths.push(`/posts/${slug}`)
+  }
 
   const locs = paths.map(
     (loc) => `<url><loc>https://www.${DOMAIN}${loc}</loc></url>`
