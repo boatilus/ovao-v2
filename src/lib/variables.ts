899ebda1a489cb963fe4562371abe411f6462082
Ron Jones <ron@ovao.dev>
2024-06-11 21:06:36 -0400

    chore: adios, Ello

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,7 +1,6 @@
 export const DOMAIN = import.meta.env.VITE_PUBLIC_DOMAIN as string
 export const TITLE = import.meta.env.VITE_PUBLIC_TITLE as string
 export const GITHUB_URL = import.meta.env.VITE_PUBLIC_GITHUB_URL as string
-export const ELLO_URL = import.meta.env.VITE_PUBLIC_ELLO_URL as string
 export const INSTAGRAM_URL = import.meta.env.VITE_PUBLIC_INSTAGRAM_URL as string
 export const LINKEDIN_URL = import.meta.env.VITE_PUBLIC_LINKEDIN_URL as string
 export const GOOGLE_MEASUREMENT_ID = import.meta.env
